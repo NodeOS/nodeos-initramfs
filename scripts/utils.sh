0d8d7cbf7b3edfec3a474b1009339b2d9852d584
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2016-12-06 08:35:22 +0100

    Use `libfuse` package

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,7 +1,10 @@
+NODE_DIR=`node -p "require('nodeos-nodejs')"`
+
 NPMi='CC=$TARGET-gcc              \
       CXX=$TARGET-g++             \
       npm_config_prefix=$STEP_DIR \
       npm install                 \
+        --production              \
         --quiet --no-spin         \
         --arch=$NODE_ARCH         \
         --nodedir=$NODE_DIR       \
